e7d1d92bbbb173915d04ae3b1f1fb3a0ded39f10
anonuse <54348285+anonuse@users.noreply.github.com>
2021-03-21 22:32:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //typing animation
 var typed = new Typed(".typing", {
-    strings: ["Alan Wong.", "a Hong Konger."],
+    strings: ["Alan Wong.", "a Hong Konger.", "an IT Dog."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
